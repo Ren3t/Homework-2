4f1e3be0ef3c35a8fed93c920f4c79383cf9c635
Ren3t <ren3t.eres@gmail.com>
2019-10-12 19:59:32 +0300

    Task4 complete

--- res/js/script.js
+++ res/js/script.js
@@ -1,15 +1,24 @@
 $(document).ready(function() {
     $(function () {
+        //Make "Profile " and "Courses" buttons clickable so that they show/hide corresponding containers
+        //Courses
         $("#courses-button").click(function () {
             $('#courses-container').removeClass("tab").addClass("active");
             $('#profile-button').removeClass("pill active").addClass("pill");
             $('#courses-button').addClass("pill active");
         });
-
+        //Profile
         $("#profile-button").click(function () {
             $('#courses-container').removeClass("active").addClass("tab");
             $('#courses-button').removeClass("pill active").addClass("pill");
             $('#profile-button').addClass("pill active");
         });
+
+        //Make "+"  button clickable so that it shows/hides form to add new course
+        $("#add-course-button").click(function () {
+            $('#add-course').toggle()
+        });
     });
+
+
 });
