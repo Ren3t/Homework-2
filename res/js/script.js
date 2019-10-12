ce58f5703d31b79190172be9a509d3e22c2fa681
Erik Marcus Maks <makserik@hotmail.com>
2019-10-12 22:11:53 +0300

    Added classes and course init.

--- res/js/script.js
+++ res/js/script.js
@@ -1,8 +1,47 @@
 $(document).ready(function() {
+	var user = new User(
+	"John",
+	"Doe",
+	"11/10/1990",
+	"Software Engineering",
+	"2.75"
+	)
+	var courses = [];
+	var course1 = new Course(
+	"Agile software development",
+	"1",
+	"82",
+	)
+	var course2 = new Course(
+	"System modeling",
+	"1",
+	"85"
+	)
+	var course3 = new Course(
+	"Object-oriented programming",
+	"2",
+	"99"
+	)
+	var course4 = new Course(
+	"Estonian language level A2",
+	"2",
+	"65"
+	)
+	courses[0] = course1;
+	courses[1] = course2;
+	courses[2] = course3;
+	courses[3] = course4;
     $(function () {
         //Make "Profile " and "Courses" buttons clickable so that they show/hide corresponding containers
         //Courses
         $("#courses-button").click(function () {
+			//Initialize courses table
+			for(var i = 0;i<courses.length;i++){
+				//$("#courses-container courses")."<td>"+(i+1).toString()+"</td>";
+				$("#courses-container courses")."<td>"courses[i].title+"</td>";
+				$("#courses-container courses")."<td>"courses[i].semester+"</td>";
+				$("#courses-container courses")."<td>"courses[i].grade    +"</td>";
+			}
             $('#courses-container').removeClass("tab").addClass("active");
             $('#profile-button').removeClass("pill active").addClass("pill");
             $('#courses-button').addClass("pill active");
@@ -18,6 +57,7 @@ $(document).ready(function() {
         $("#add-course-button").click(function () {
             $('#add-course').toggle()
         });
+	
     });
 
 
