fa49eca42b1fd6d19f5645f03d73b398336cc24a
Ren3t <ren3t.eres@gmail.com>
2019-10-12 19:29:54 +0300

    Task3 complete

--- res/js/script.js
+++ res/js/script.js
@@ -1,7 +1,15 @@
-$( function() {
-    $( "#courses-button" ).click( function() {
-        $('#courses-container').removeClass("tab").addClass("active");
-        $('.pill.active').removeClass("pill active").add("pill");
-        $('.pill').addClass("pill active");
-    } );
+$(document).ready(function() {
+    $(function () {
+        $("#courses-button").click(function () {
+            $('#courses-container').removeClass("tab").addClass("active");
+            $('#profile-button').removeClass("pill active").addClass("pill");
+            $('#courses-button').addClass("pill active");
+        });
+
+        $("#profile-button").click(function () {
+            $('#courses-container').removeClass("active").addClass("tab");
+            $('#courses-button').removeClass("pill active").addClass("pill");
+            $('#profile-button').addClass("pill active");
+        });
+    });
 });
